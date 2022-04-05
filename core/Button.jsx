93a5eb365249f573569238956010aa6f50a21fe6
문어햄 <60453738+HyeBinYang@users.noreply.github.com>
2022-04-05 21:30:12 +0900

    Deploy

---
+++ core/Button.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Wrapper = styled.div`
+  background-color: red;
+  color: white;
+`;
+
+const Button = () => {
+  return <Wrapper>Button</Wrapper>;
+};
+
+export default Button;
